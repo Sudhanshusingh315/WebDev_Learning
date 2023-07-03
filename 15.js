c75b4c3af54b72554ee329b06d7df6daab44d18c
LeaderOfMeow <ss826063@gmail.com>
2023-07-03 21:37:26 +0530

    fourth commiut

---
+++ 15.js
@@ -0,0 +1,14 @@
+// Practice set of strings
+
+/* Q1 what will the following print in javascript?
+
+    console.log("har\"".length);
+    ANS : 4
+*/ 
+
+
+/*
+Q4  */
+
+let str2 = "Please give Rs 1000"
+let amount =
