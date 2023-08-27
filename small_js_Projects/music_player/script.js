c1f701f39dfbda2b849893d2210f5cd5450473ee
Sudhanshu singh <ss826063@gmail.com>
2023-08-27 23:44:39 +0530

    wokring progress bar

--- small_js_Projects/music_player/script.js
+++ small_js_Projects/music_player/script.js
@@ -10,10 +10,18 @@ const playButton = document.querySelector("#play");
 const pauseButton = document.querySelector("#pause");
 const volumeScroller = document.querySelector("#volume-scroller");
 
+// defining progress bar
+
+const progress = document.querySelector(".progress");
+
+// starts coding here
+
 playButton.addEventListener("click", () => {
   playing_audio.play();
   console.log("Song started playing");
-  console.log("Buffer: ",playing_audio.buffered)
+  setInterval(function () {
+    updatingbar(value);
+  }, 1000);
 });
 
 pauseButton.addEventListener("click", () => {
@@ -28,11 +36,26 @@ volumeScroller.addEventListener("input", () => {
   console.log(newValue);
 });
 
+// showing current time and playing porgress bar along with it
+
 setInterval(() => {
   console.log(
     `current time/duration: ${Math.floor(
       playing_audio.currentTime
     )}/${Math.floor(playing_audio.duration / 60)}:00 mins`
   );
+  console.log("Buffer: ", playing_audio.buffered);
 }, 1000);
+
+// Creating a progress bar
+
+
+
+let value = 0;
+const updatingbar = () => {
+  let progress = document.querySelector(".progress");
+  progress.firstElementChild.style.width = `${value}%`;
+  value++;
+  console.log(progress);
+};
 
