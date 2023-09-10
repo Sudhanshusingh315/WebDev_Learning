6c2f8966fcea6bb1d62a79bff2d1f4d2b00e6dbf
Sudhanshu singh <ss826063@gmail.com>
2023-09-10 20:06:21 +0530

    finished memory project

--- small_js_Projects/memory_project/script.js
+++ small_js_Projects/memory_project/script.js
@@ -1,11 +1,11 @@
 const cardArray = [
     {
         name:"cheeseburger",
-        img : 'images/cheesburger.png'
+        img : 'images/cheeseburger.png'
     },
     {
         name:"fries",
-        img: 'images/friens.png'
+        img: 'images/fries.png'
     },
     {
         name:"hotdog.png",
@@ -17,18 +17,19 @@ const cardArray = [
     },
     {
         name:"milkshakes",
-        img:'imgaes/milkshakes.png'
+        img:'images/milkshake.png'
     },
     {
         name:"pizza",
         img:'images/pizza.png'
     },
     {
-        img : 'images/cheesburger.png'
+        name:"cheeseburger", 
+        img : 'images/cheeseburger.png'
     },
     {
         name:"fries",
-        img: 'images/friens.png'
+        img: 'images/fries.png'
     },
     {
         name:"hotdog.png",
@@ -40,7 +41,7 @@ const cardArray = [
     },
     {
         name:"milkshakes",
-        img:'imgaes/milkshakes.png'
+        img:'images/milkshake.png'
     },
     {
         name:"pizza",
@@ -48,9 +49,13 @@ const cardArray = [
     },
 ];
 
+// shuffling this array now
 cardArray.sort(() => Math.random() - 0.5);
 // console.log(cardArray);
-
+let cardsChosenIds = [];
+let cardsChosen = [];
+let score = 0;
+let win =[];
 const cardDisplay = document.querySelector('#grid');
 let card ;
 
@@ -59,16 +64,68 @@ const creatBoard = () => {
         card = document.createElement('img'); 
         card.setAttribute('src','images/blank.png');
         card.setAttribute('data-id',i);
-        card.addEventListener('click',flipcard) 
         cardDisplay.appendChild(card);
+        card.addEventListener('click',flipcard) 
         console.log(card);
-   } 
+    } 
 }
+
 
 creatBoard();
 
 // creating a flipping board
 
 function flipcard(){
-    console.log('clicked');
+    const cardId = this.getAttribute("data-id")
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenIds.push(cardId);
+    this.setAttribute('src',cardArray[cardId].img);
+    console.log(cardsChosenIds);
+    checkForMatch();
+    iswin(); 
+    
 }
+
+function checkForMatch(){
+    if(cardsChosen.length === 2){
+        setTimeout(isMatch,200)
+    }
+    
+}
+
+function isMatch(){
+    let card = document.querySelectorAll('#grid img');
+    // console.log(card);
+    if(cardsChosen[0] === cardsChosen[1]){
+        win.push[cardsChosen]; 
+        console.log("matched");
+        card[cardsChosenIds[0]].setAttribute('src','images/white.png'); 
+        card[cardsChosenIds[1]].setAttribute('src','images/white.png'); 
+        card[cardsChosenIds[0]].removeEventListener('click',flipcard);
+        
+        card[cardsChosenIds[1]].removeEventListener('click',flipcard);
+        cardsChosenIds= [];
+        cardsChosen = [];
+
+    }
+    
+    if(cardsChosen[0] != cardsChosen[1]){
+        
+        card[cardsChosenIds[0]].setAttribute('src','images/blank.png');
+        card[cardsChosenIds[1]].setAttribute('src','images/blank.png');
+        cardsChosen = [];
+        cardsChosenIds= [];
+        console.log("did not match,resetting")
+    }
+        
+}
+
+function iswin(){
+    console.log("size of win is",win);
+
+}
+
+
+
+
+
