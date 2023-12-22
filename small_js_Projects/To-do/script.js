71ebe12d567f3b2d23f04a097de59956d605854a
LeaderOfMeow <ss826063@gmail.com>
2023-12-22 13:50:50 +0530

    To-do list finish

--- small_js_Projects/To-do/script.js
+++ small_js_Projects/To-do/script.js
@@ -1,1 +1,32 @@
-console.log("works");
+const inputBox = document.getElementById("input-tag");
+const listContainer = document.getElementById("list-container");
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click",()=>{
+    if(inputBox.value === ''){
+        alert("You must enter something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span =  document.createElement("span");
+        span.innerHTML = "x";
+        li.appendChild(span);
+
+    }
+
+    inputBox.value = '';
+})
+
+listContainer.addEventListener("click",(e)=>{
+    console.log(e);
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+})
