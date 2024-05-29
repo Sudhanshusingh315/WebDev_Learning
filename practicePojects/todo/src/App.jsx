d75d82e713e5a6d016756a2646b25865223def1e
LeaderOfMeow <ss826063@gmail.com>
2024-05-29 12:28:58 +0530

    auth project and todo list

---
+++ practicePojects/todo/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todo, setTodo] = useState([]);
+  const [counter,setCoutner] = useState(0); 
+  const [input, setInput] = useState("");
+  const addItemstotheList = () => {
+    let newValue = {
+      id: Number(counter),
+      liValue : input
+    }
+    setTodo((perv)=>{return [...perv,newValue]});
+    setCoutner((prev)=>prev+1);
+    setInput("");
+  } 
+  return (
+    <>
+      <h1>To do list</h1>
+      <input type='text' name='inputeValue' value={input} placeholder='enter an item here' onChange={(e) => { setInput(e.target.value) }} />
+      <button onClick={() => { addItemstotheList() }}>Add me</button>
+      <div>
+        {counter>0 && todo.map((items,index)=>{
+          return <li key={`key-${index}`}>{items.liValue}</li>
+        })}
+      </div>
+    </>
+  )
+}
+
+export default App
