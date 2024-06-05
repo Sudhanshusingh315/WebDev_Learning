7b20be6f28c145236db535c45aa403cdb2540dcc
LeaderOfMeow <ss826063@gmail.com>
2024-06-05 12:24:53 +0530

    machine coding model

---
+++ practicePojects/modale/src/components/Model.jsx
@@ -0,0 +1,13 @@
+import './model.css';
+export default function Model(){
+    return (
+        <>
+        <div className='container'>
+            <div>x</div>
+            <div>Hello i&apos;m meow</div>
+            <div>O</div>
+
+        </div>
+        </>
+    )
+}
