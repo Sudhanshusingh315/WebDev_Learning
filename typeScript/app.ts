9be6cf4855fca786388385be09c6c05952e23783
LeaderOfMeow <ss826063@gmail.com>
2024-07-05 23:27:22 +0530

    itnerfaces and union

--- typeScript/app.ts
+++ typeScript/app.ts
@@ -51,6 +51,103 @@
 // }
 
 
-type ID = number | string;
+// type ID = number | string;
 
-const userId:ID = "userID"
+// const userId:ID = "userID"
+
+
+
+// --------------------------------- Interfaces ---------------------------------------
+
+// there is no eqaul sign in interfaces
+interface Transaction {
+    payerAccountNumber : number;
+    payeeAccountNumber : number;
+}
+
+interface BackAccount {
+    accountNumber:number;
+    accountHolder : string;
+    balance: number;
+    isActive:boolean;
+    transaction: Transaction[]
+
+}
+
+// Transactions
+const transaction1 : Transaction = {
+    payerAccountNumber:123,
+    payeeAccountNumber:321
+}
+
+const transaction2 : Transaction = {
+    payerAccountNumber:213,
+    payeeAccountNumber:132
+}
+
+const bankAccount:BackAccount = {
+    accountNumber:123,
+    accountHolder:"Jhon doe",
+    balance: 4000,
+    isActive:true,
+    transaction :[transaction1,transaction2]
+}
+
+
+//  Extending interfaces
+
+interface Book {
+    name:string;
+    price:number;
+}
+
+interface Ebook extends Book{
+    format:string;
+    typeOfBook:string;
+}
+
+interface AudioBook extends Book,Ebook{
+    duration:number;
+}
+
+
+const book:Book= {
+    name:'Atomic habits',
+    price:1200
+}
+
+const Ebook1 : Ebook = {    
+    name:'Ikagi',
+    price:100,
+    format:'pdf',
+    typeOfBook:"Black and White"
+}
+
+const AudioBook1 : AudioBook = {
+    name:'Ikagi',
+    price:100,
+    format:'dox',
+    typeOfBook:'Color',
+    duration:1
+}
+
+// Unions
+
+type ID = number | string
+
+function prindId(id:ID){
+    console.log(id)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
