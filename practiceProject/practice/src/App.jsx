d719309f7bceceb83abf5ef4dd066ea305deb187
Sudhanshu singh <ss826063@gmail.com>
2025-04-09 19:23:50 +0530

    futile attempt of accordian

--- practiceProject/practice/src/App.jsx
+++ practiceProject/practice/src/App.jsx
@@ -1,24 +1,38 @@
-import { useEffect } from "react";
-import "./App.css";
 import { useState } from "react";
-import { useRef } from "react";
-import Photos from "./Photos";
+import "./App.css";
+import data from "./data.json";
 
 function App() {
-    const [imageData, setImagesData] = useState(null);
-    const [pageNo, setPageNo] = useState(1);
-    useEffect(() => {
-        fetch(`https://picsum.photos/v2/list?page=${pageNo}&limit=10`)
-            .then((res) => res.json())
-            .then((data) => {
-                setImagesData(data);
-            });
-    }, []);
-
+    console.log("data", data);
+    const [show, setShow] = useState();
+    const handleAccordian = (index) => {
+        setShow(index);
+    };
     return (
-        <>
-            <Photos imageData={imageData} setImagesData={setImagesData} />
-        </>
+        <div className="wrapper">
+            {data?.map(({ question, answer }, index) => {
+                return (
+                    <div
+                        className="accordian"
+                        onClick={() => {
+                            handleAccordian(index);
+                        }}
+                        key={index}
+                    >
+                        <div className="title">
+                            <h3>{question}</h3>
+                            <span>+</span>
+                        </div>
+
+                        {index === show && (
+                            <div className="content">
+                                <p>{answer}</p>
+                            </div>
+                        )}
+                    </div>
+                );
+            })}
+        </div>
     );
 }
 
