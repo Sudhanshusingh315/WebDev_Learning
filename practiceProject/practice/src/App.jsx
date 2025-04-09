51cec277358307dfa6054fcb1c9bda311d13ce9c
Sudhanshu singh <ss826063@gmail.com>
2025-04-09 12:18:55 +0530

    practice otp

--- practiceProject/practice/src/App.jsx
+++ practiceProject/practice/src/App.jsx
@@ -1,30 +1,59 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import PhotoControl from "./PhotoControl";
+import { useRef } from "react";
 
-function App() {
-    const [photos, setPhotos] = useState(null);
-    const [pageNo, setPageNo] = useState(1);
-    useEffect(() => {
-        fetch(`https://picsum.photos/v2/list?page=${pageNo}&limit=5`)
-            .then((res) => res.json())
-            .then((res) => {
-                console.log("final response", res);
-                setPhotos(res);
-            });
-    }, [pageNo]);
+function App({ otpFileds = 6 }) {
+    const [otp, setOtps] = useState(Array(otpFileds).fill(""));
+    const otpInputRefs = useRef([]);
+    console.log("otpInput", otpInputRefs);
+    console.log("otp value", otp);
+    const hadleOtpInput = (e, index) => {
+        const { key } = e;
+        console.log(key);
+        switch (key) {
+            case "Backspace":
+                setOtps((prev) => {
+                    const temp = [...prev];
+                    temp.splice(index, 1, "");
+                    return temp;
+                });
+                if (index > 0) {
+                    otpInputRefs.current[index - 1].focus();
+                }
+                break;
 
+            default:
+                break;
+        }
+
+        if (isNaN(key)) return;
+
+        // figure out why didn't setOpt(prev) worked?
+        console.log("key", key);
+        const newOtp = [...otp];
+        newOtp[index] = key;
+        setOtps(newOtp);
+        if (index < otp.length - 1) {
+            otpInputRefs?.current[index + 1].focus();
+        }
+    };
     return (
         <>
-            <div className="photoGrid">
-                {photos?.map(({ id, download_url }) => {
+            <div className="otp-fileds">
+                {otp?.map((value, index) => {
                     return (
-                        <img key={id} src={download_url} alt="photo-gallery" />
+                        <input
+                            ref={(currentInput) => {
+                                otpInputRefs.current[index] = currentInput;
+                            }}
+                            key={index}
+                            value={value}
+                            type="text"
+                            onKeyDown={(e) => hadleOtpInput(e, index)}
+                        />
                     );
                 })}
             </div>
-            {/* photos control */}
-            <PhotoControl pageNo={pageNo} setPageNo={setPageNo} />
         </>
     );
 }
