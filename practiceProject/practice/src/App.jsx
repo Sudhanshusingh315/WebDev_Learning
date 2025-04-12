96755bd5435bd0d828353f7afb19eec8e6748f7f
Sudhanshu singh <ss826063@gmail.com>
2025-04-12 17:04:42 +0530

    use custom hooks

--- practiceProject/practice/src/App.jsx
+++ practiceProject/practice/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import "./App.css";
-import Tab from "./Tabs";
+import useCustomMemo from "./useCustomMemo";
 
+// custom useMmemo
 export default function App() {
-    const [currentTabIndex, setCurrentTabIndex] = useState(1);
-    console.log("current tab index", currentTabIndex);
-    const handleIndex = (index) => {
-        setCurrentTabIndex(index);
-    };
+    const [counter, setCounter] = useState(0);
+    const [input, setInput] = useState("");
+    function expensiveCalculation(num) {
+        console.log("running expensive calculation");
+        for (let i = 0; i <= 100; i++) {}
+        return num * 2;
+    }
+
+    const doubleValue = useCustomMemo(
+        () => expensiveCalculation(counter),
+        [counter]
+    );
     return (
         <div>
-            <Tab currentTab={currentTabIndex} onChange={handleIndex}>
-                <Tab.HeadsContainer>
-                    <Tab.Item lable="Tab 1" index={1} />
-                    <Tab.Item lable="Tab 2" index={2} />
-                    <Tab.Item lable="Tab 3" index={3} />
-                </Tab.HeadsContainer>
-                <Tab.Container>
-                    <Tab.ContainerContent index={1}>
-                        content 1
-                    </Tab.ContainerContent>
-                    <Tab.ContainerContent index={2}>
-                        content 2
-                    </Tab.ContainerContent>
-                    <Tab.ContainerContent index={3}>
-                        content 3
-                    </Tab.ContainerContent>
-                </Tab.Container>
-            </Tab>
+            <h2>Count: {counter}</h2>
+            <h4>Count: {doubleValue}</h4>
+            <button onClick={() => setCounter((prev) => prev + 1)}>
+                increment
+            </button>
+            <input
+                placeholder="type something"
+                value={input}
+                onChange={(e) => {
+                    setInput(e.target.value);
+                }}
+            />
         </div>
     );
 }
