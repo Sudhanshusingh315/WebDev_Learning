89b493f05fe543d134e53bc90b900fca4b347ce7
Sudhanshu singh <ss826063@gmail.com>
2025-04-17 17:36:58 +0530

    css day speakers

--- practiceProject/test.js
+++ practiceProject/test.js
@@ -1,23 +1,8 @@
-const arry = [20, 41, 22, [32, 56, 89], [[123, 2, 56, 77]]];
+const data = new Promise((resolve, reject) => {
+    resolve(1);
+    resolve(2);
+    resolve(3);
+    reject(3);
+});
 
-Array.prototype.myFlat = function (level = 1) {
-    const result = [];
-
-    function flatten(array, currentLevel) {
-        for (let i = 0; i < array.length; i++) {
-            if (Array.isArray(array[i]) && currentLevel < level) {
-                flatten(array[i], currentLevel + 1);
-            } else {
-                result.push(array[i]);
-            }
-        }
-    }
-
-    flatten(this, 0);
-
-    return result;
-};
-
-
-const data = arry.myFlat(4);
-console.log(data);
+data.then((res) => console.log(res)).catch((err) => console.log(err));
