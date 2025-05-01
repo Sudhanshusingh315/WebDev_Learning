0c2abe62aebeb6668096f37f84a826029235c148
Sudhanshu singh <ss826063@gmail.com>
2025-05-02 00:42:23 +0530

    image carousel practice

--- practiceProject/src/App.jsx
+++ practiceProject/src/App.jsx
@@ -1,128 +1,60 @@
+import { useState, useRef, use, useEffect } from "react";
 import "./App.css";
 
-import { useState, useRef, use, useEffect } from "react";
-
-const TimeFactor = {
-    Hour: "hh",
-    Minute: "mm",
-    Seconds: "ss",
-};
-//timer
-const Config = {
-    [TimeFactor.Hour]: {
-        value: "",
-        factor: 60 * 60 * 1000,
-        placeHolder: "HH",
-    },
-    [TimeFactor.Seconds]: {
-        value: "",
-        factor: 1000,
-        placeHolder: "SS",
-    },
-    [TimeFactor.Minute]: {
-        value: "",
-        factor: 60 * 1000,
-        placeHolder: "MM",
-    },
-};
-const OrderOfTime = [TimeFactor.Hour, TimeFactor.Minute, TimeFactor.Seconds];
-
+const IMAGES = [
+    "https://images.unsplash.com/photo-1735767976699-6096acda642d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1735732519861-3b67d0aee297?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1736894739274-3e85a224f350?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1736878363865-ae21e0290d8c?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1734640113825-24dd7c056052?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
 function App() {
-    const [config, setConfig] = useState(structuredClone(Config));
-    const [time, setTime] = useState(0);
-    const intervalRef = useRef(null);
-    const timeSpentRef = useRef(0);
-
-    const handleChange = (e, data) => {
-        const { key } = data;
-        setConfig((prev) => {
-            return {
-                ...prev,
-                [key]: { ...prev[key], value: e.target.value },
-            };
-        });
+    const [image, setImgage] = useState(structuredClone(IMAGES));
+    const [index, setIndex] = useState(0);
+    const handleNext = () => {
+        console.log(index);
+        if (index === image.length - 1) {
+            console.log("im inside");
+            setIndex(0);
+            return;
+        }
+        setIndex((prev) => prev + 1);
     };
-    console.log(config);
-    const handleStart = () => {
-        // convert all the time in ms
-        let totalTimeInMilliseconds = 0;
-
-        OrderOfTime.forEach((key) => {
-            const data = config[key];
-
-            const value = data.value;
-            const factor = data.factor;
-
-            // add validation maybe not rn
-            totalTimeInMilliseconds += parseInt(value) * factor;
-        });
-        console.log(totalTimeInMilliseconds);
-        // and then start the timer
-
-        timeSpentRef.current = Date.now() + totalTimeInMilliseconds;
-
-        intervalRef.current = setInterval(() => {
-            setTime(() => {
-                return timeSpentRef.current - new Date().getTime();
-            });
-        }, 10);
-    };
-    const handlePause = () => {
-        clearInterval(intervalRef.current);
-        intervalRef.current = null;
-    };
-    const handleReset = () => {
-        setTime(0);
+    const handlePrev = () => {
+        if (index === 0) {
+            setIndex(image.length - 1);
+            return;
+        }
+        setIndex((prev) => prev - 1);
     };
 
-    const formatTime = () => {
-        const ms = Math.floor((time % 1000) / 10);
-        const mm = Math.floor((time / (1000 * 60)) % 60);
-        const ss = Math.floor((time / 1000) % 60);
-        const hh = Math.floor(time / (1000 * 60 * 60));
+    useEffect(() => {
+        const interval = setInterval(() => {
+            handleNext();
+        }, 1000);
 
-        return `${hh}:${mm}:${ss}`;
-    };
+        return () => clearInterval(interval);
+    }, [handleNext]);
+
     return (
-        <>
-            <div>
-                <p>{formatTime()}</p>
-                {OrderOfTime?.map((element) => {
-                    const { factor, placeHolder, value } = config[element];
+        <div className="container">
+            <div className="carousel-container">
+                {image?.map((url) => {
                     return (
-                        <input
-                            key={element}
-                            value={value}
-                            placeholder={placeHolder}
-                            onChange={(e) => {
-                                handleChange(e, { key: element });
+                        <img
+                            src={url}
+                            key={url}
+                            style={{
+                                translate: `${-100 * index}%`,
                             }}
                         />
                     );
                 })}
             </div>
-            <button
-                onClick={() => {
-                    handleStart();
-                }}
-            >
-                start
-            </button>
-            <button
-                onClick={() => {
-                    handlePause();
-                }}
-            >
-                pause
-            </button>
-            <button
-                onClick={() => {
-                    handleReset();
-                }}
-            >
-                restart
-            </button>
-        </>
+
+            <button onClick={handleNext}>N</button>
+            <button onClick={handlePrev}>P</button>
+        </div>
     );
 }
 
