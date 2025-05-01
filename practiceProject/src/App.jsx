13d6ff5f6b61397d0a3a128a7b32e357f702c1c4
Sudhanshu singh <ss826063@gmail.com>
2025-05-01 21:04:46 +0530

    timer, reverse counter

--- practiceProject/src/App.jsx
+++ practiceProject/src/App.jsx
@@ -1,47 +1,128 @@
 import "./App.css";
 
-import { useState, useRef, use } from "react";
+import { useState, useRef, use, useEffect } from "react";
 
-// stop watch
+const TimeFactor = {
+    Hour: "hh",
+    Minute: "mm",
+    Seconds: "ss",
+};
+//timer
+const Config = {
+    [TimeFactor.Hour]: {
+        value: "",
+        factor: 60 * 60 * 1000,
+        placeHolder: "HH",
+    },
+    [TimeFactor.Seconds]: {
+        value: "",
+        factor: 1000,
+        placeHolder: "SS",
+    },
+    [TimeFactor.Minute]: {
+        value: "",
+        factor: 60 * 1000,
+        placeHolder: "MM",
+    },
+};
+const OrderOfTime = [TimeFactor.Hour, TimeFactor.Minute, TimeFactor.Seconds];
+
 function App() {
+    const [config, setConfig] = useState(structuredClone(Config));
     const [time, setTime] = useState(0);
-    const [timerStarted, setTimerStared] = useState(false);
-    const stopWatchRef = useRef(0);
-    const intervalRef = useRef(0);
+    const intervalRef = useRef(null);
+    const timeSpentRef = useRef(0);
 
+    const handleChange = (e, data) => {
+        const { key } = data;
+        setConfig((prev) => {
+            return {
+                ...prev,
+                [key]: { ...prev[key], value: e.target.value },
+            };
+        });
+    };
+    console.log(config);
     const handleStart = () => {
-        const stopWatchRef = Date.now() - time;
-        // only want this to stop the time
+        // convert all the time in ms
+        let totalTimeInMilliseconds = 0;
+
+        OrderOfTime.forEach((key) => {
+            const data = config[key];
+
+            const value = data.value;
+            const factor = data.factor;
+
+            // add validation maybe not rn
+            totalTimeInMilliseconds += parseInt(value) * factor;
+        });
+        console.log(totalTimeInMilliseconds);
+        // and then start the timer
+
+        timeSpentRef.current = Date.now() + totalTimeInMilliseconds;
+
         intervalRef.current = setInterval(() => {
-            setTime(Date.now() - stopWatchRef);
+            setTime(() => {
+                return timeSpentRef.current - new Date().getTime();
+            });
         }, 10);
     };
     const handlePause = () => {
         clearInterval(intervalRef.current);
+        intervalRef.current = null;
     };
     const handleReset = () => {
-        clearInterval(intervalRef.current);
         setTime(0);
     };
+
     const formatTime = () => {
-        let ms = String(Math.floor((time % 1000) / 10)).padStart(2, "0");
-        let ss = String(Math.floor((time / 1000) % 60)).padStart(2, "0");
-        let mm = String(Math.floor((time / (1000 * 60)) % 60)).padStart(2, "0");
-        let hh = String(Math.floor((time / (1000 * 60 * 60)) % 24)).padStart(
-            2,
-            "0"
-        );
+        const ms = Math.floor((time % 1000) / 10);
+        const mm = Math.floor((time / (1000 * 60)) % 60);
+        const ss = Math.floor((time / 1000) % 60);
+        const hh = Math.floor(time / (1000 * 60 * 60));
 
-        return `${hh}:${mm}:${ss}:${ms}`;
+        return `${hh}:${mm}:${ss}`;
     };
-
     return (
-        <div>
-            <div>{formatTime()}</div>
-            <button onClick={handleStart}>Start</button>
-            <button onClick={handlePause}>Pause</button>
-            <button onClick={handleReset}>Reset</button>
-        </div>
+        <>
+            <div>
+                <p>{formatTime()}</p>
+                {OrderOfTime?.map((element) => {
+                    const { factor, placeHolder, value } = config[element];
+                    return (
+                        <input
+                            key={element}
+                            value={value}
+                            placeholder={placeHolder}
+                            onChange={(e) => {
+                                handleChange(e, { key: element });
+                            }}
+                        />
+                    );
+                })}
+            </div>
+            <button
+                onClick={() => {
+                    handleStart();
+                }}
+            >
+                start
+            </button>
+            <button
+                onClick={() => {
+                    handlePause();
+                }}
+            >
+                pause
+            </button>
+            <button
+                onClick={() => {
+                    handleReset();
+                }}
+            >
+                restart
+            </button>
+        </>
     );
 }
 
