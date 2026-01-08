6cca1869b5ba0e77f2b496e25b5ffe6c2a554b0f
leaderofmeow <ss826063@gmail.com>
2026-01-08 23:16:28 +0530

    restaring go and tab-with-lazy-loading

--- go-learning/1/helloworld.go
+++ go-learning/1/helloworld.go
@@ -2,8 +2,22 @@ package main
 
 import "fmt"
 
-// entry point of go lang
 func main() {
-	fmt.Println("Hello world")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("welcome to %v booking application\n", conferenceName)
+
+	fmt.Printf("We have total of %v and %v are still available", conferenceName, remainingTickets)
+
+	fmt.Println("get your tickets here to attend")
+
+	var userName string
+
+	// ask user for their name to enter
+
+	userName = "Tome"
+	fmt.Printf("userName is %v \n", userName)
 
 }
